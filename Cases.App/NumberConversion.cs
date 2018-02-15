95e0b336c456bf540220bf0dea400830fd3795cc
Brooke Lewis <belv2c@gmail.com>
2018-02-14 19:33:31 -0600

    finished assignment and all tests pass

---
+++ Cases.App/NumberConversion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cases.App
+{
+
+    public class NumberConversion
+    {
+
+        public string ToRoman(int number)
+        {
+            var returnValue = new StringBuilder(5);
+            var numberDictionary = new SortedDictionary<int, string>
+        {
+            { 1, "I" },
+            { 4, "IV" },
+            { 5, "V" },
+            { 9, "IX" },
+            { 10, "X" },
+            { 40, "XL" },
+            { 50, "L" },
+            { 90, "XC" },
+            { 100, "C" },
+            { 400, "CD" },
+            { 500, "D" },
+            { 900, "CM" },
+            { 1000, "M" },
+        };
+
+            foreach (var intRomanPair in numberDictionary.Reverse())
+            {
+                while (number >= intRomanPair.Key)
+                {
+                    number -= intRomanPair.Key;
+                    returnValue.Append(intRomanPair.Value);
+                }
+            }
+
+            return returnValue.ToString();
+        }
+    }
+}
+
+
+
---
+++ Cases.Test/Class1.cs
@@ -0,0 +1,143 @@
+﻿using NUnit.Framework;
+using Cases.App;
+
+namespace Cases.Test
+{
+    [TestFixture]
+    public class RomanNumeralConversionTests
+    {
+
+        [Test]
+        public void Convert_Number_One_To_Roman_Numeral_I()
+        {
+            var numberConversion = new NumberConversion();
+            var results = numberConversion.ToRoman(1);
+            var expected = "I";
+
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Convert_Number_Four_To_Roman_Numeral_IV()
+        {
+            var numberConversion = new NumberConversion();
+            var results = numberConversion.ToRoman(4);
+            var expected = "IV";
+
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Convert_Number_Five_To_Roman_Numeral_V()
+        {
+            var numberConversion = new NumberConversion();
+            var results = numberConversion.ToRoman(5);
+            var expected = "V";
+
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Convert_Number_Nine_To_Roman_Numeral_IX()
+        {
+            var numberConversion = new NumberConversion();
+            var results = numberConversion.ToRoman(9);
+            var expected = "IX";
+
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Convert_Number_Ten_To_Roman_Numeral_X()
+        {
+            var numberConversion = new NumberConversion();
+            var results = numberConversion.ToRoman(10);
+            var expected = "X";
+
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Convert_Number_Fourty_To_Roman_Numeral_XL()
+        {
+            var numberConversion = new NumberConversion();
+            var results = numberConversion.ToRoman(40);
+            var expected = "XL";
+
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Convert_Number_Fifty_To_Roman_Numeral_L()
+        {
+            var numberConversion = new NumberConversion();
+            var results = numberConversion.ToRoman(50);
+            var expected = "L";
+
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Convert_Number_Ninety_To_Roman_Numeral_XC()
+        {
+            var numberConversion = new NumberConversion();
+            var results = numberConversion.ToRoman(90);
+            var expected = "XC";
+
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Convert_Number_One_Hundred_To_Roman_Numeral_C()
+        {
+            var numberConversion = new NumberConversion();
+            var results = numberConversion.ToRoman(100);
+            var expected = "C";
+
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Convert_Number_Four_Hundred_To_Roman_Numeral_CD()
+        {
+            var numberConversion = new NumberConversion();
+            var results = numberConversion.ToRoman(400);
+            var expected = "CD";
+
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Convert_Number_Five_Hundred_To_Roman_Numeral_D()
+        {
+            var numberConversion = new NumberConversion();
+            var results = numberConversion.ToRoman(500);
+            var expected = "D";
+
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Convert_Number_Nine_Hundred_To_Roman_Numeral_CM()
+        {
+            var numberConversion = new NumberConversion();
+            var results = numberConversion.ToRoman(900);
+            var expected = "CM";
+
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Convert_Number_One_Thousand_To_Roman_Numeral_M()
+        {
+            var numberConversion = new NumberConversion();
+            var results = numberConversion.ToRoman(1000);
+            var expected = "M";
+
+            Assert.AreEqual(expected, results);
+        }
+    }
+}
+
+  
+
